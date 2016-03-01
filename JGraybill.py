5d3b0acdfe4fce0b102604d7d70e25869b5e4545
loganmarsden17 <loganmarsden17@w-delaware.k12.ia.us>
2016-03-01 12:59:47 -0600

    My edit

---
+++ JGraybill.py
@@ -0,0 +1,11 @@
+# IPython log file
+
+%cd "C:\Users\NICC\Documents\GitHub\Spring2016"
+play_tournament(4)
+# Mon, 29 Feb 2016 12:52:13
+play_tournament(4)
+# Mon, 29 Feb 2016 12:53:07
+%run "C:\Users\NICC\Documents\GitHub\Spring2016\prisoners_dilemma.py"# Mon, 29 Feb 2016 12:53:18
+play_tournament(4)
+# Mon, 29 Feb 2016 13:08:59
+%logstop -ort JGraybill.py
